b4d7a019618d032759366452679a816649b42864
Michael Aylmer <michaelaylmer@Michaels-MacBook-Air.local>
2023-09-09 13:57:58 -0400

    refactored the GET/:id route to use promise.all() instead of awaiting seperate queries.

--- VideoCode/routes/users.js
+++ VideoCode/routes/users.js
@@ -21,21 +21,53 @@ router.get("/", async function (req, res, next) {
   }
 });
 
+// router.get('/:id', async (req, res, next) => {
+//   const { id } = req.params;
+//   try {
+//     // query to get user information
+//     const userResults = await db.query(`SELECT name, type FROM users WHERE id = $1`, [id]);
+//     // query to get messages information that corresponds to users info.
+//     const msgResults = await db.query(`SELECT id, msg FROM messages WHERE user_id = $1`, [id]);
+//     // error handler to check if id is valid.
+//     if (userResults.rows.length === 0) {
+//       throw new ExpressError("User not found", 404);
+//     }
+//     // object to grab first result of usersResults query.
+//     const user = userResults.rows[0];
+//     // set new propert messages equal to what we get back from msgResults query. 
+//     user.messages = msgResults.rows;
+//     return res.send(user)
+//   } catch (e) {
+//     return next(e)
+//   }
+// })
+
+// refactored way with promise.all().
 router.get('/:id', async (req, res, next) => {
   const { id } = req.params;
   try {
-    const userResults = await db.query(`SELECT name, type FROM users WHERE id = $1`, [id]);
-    const msgResults = await db.query(`SELECT id, msg FROM messages WHERE user_id = $1`, [id]);
+    // Use Promise.all to execute both queries concurrently
+    const [userResults, msgResults] = await Promise.all([
+      db.query(`SELECT name, type FROM users WHERE id = $1`, [id]),
+      db.query(`SELECT id, msg FROM messages WHERE user_id = $1`, [id])
+    ]);
+
+    // Check if the user is not found
     if (userResults.rows.length === 0) {
       throw new ExpressError("User not found", 404);
     }
+
+    // Extract user data from the first query result
     const user = userResults.rows[0];
+
+    // Attach messages to the user object
     user.messages = msgResults.rows;
-    return res.send(user)
+
+    return res.send(user);
   } catch (e) {
-    return next(e)
+    return next(e);
   }
-})
+});
 
 /** Get user: {name, type, messages: [{msg, msg}]} */
 
